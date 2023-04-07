5031204a6c8c58399faa91d41d4d0cb1d487103f
Özgür Barış Ayhan <64589883+ayhanozgurbaris@users.noreply.github.com>
2023-04-07 16:35:29 +0300

    Add files via upload

---
+++ PracticeDay3/workshop3Pair7/src/Main.java
@@ -0,0 +1,35 @@
+import Entity.Student;
+import DataAccess.StudentManager;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student student1 = new Student("Ali","Veli",1,1,1);
+        Student student2 = new Student("Ayşe","Özge",2,2,2);
+        Student student3 = new Student("Suphi","thr",2,3,3);
+        Student student4 = new Student("Suphiye","blknsasa",4,4,4);
+        Student student5 = new Student("murat","seli",6,4,5);
+        Student student6 = new Student("kemal","kamil",7,6,6);
+
+
+        StudentManager studentManager = new StudentManager();
+
+        studentManager.ekle(student1);
+        studentManager.ekle(student2);
+        studentManager.ekle(student3);
+        studentManager.ekle(student4);
+        studentManager.ekle(student5);
+        studentManager.ekle(student6);
+
+
+        studentManager.list();
+
+        studentManager.delete(student1);
+        studentManager.list();
+
+    }
+}
+
+//özgür barış ayhan
+//mert can aktaş
+//uğur atasoy
